d8f8810c4743364216af4069b50700ecc6e8e77b
stele99 <epple@mast-computer.de>
2022-09-16 16:53:37 +0200

    update1
    update1

---
+++ README.md
@@ -0,0 +1,5 @@
+# Photovolatik Forecast Module for IP-Symcon with KI 
+PV Forecast with Neuronal Network for IP Symcon
+
+___
+(c) 2022 stele99
---
+++ kiPVForecast/module.php
@@ -0,0 +1,32 @@
+<?
+
+class kiPVForecast extends IPSModule
+{
+
+	public function Create()
+	{
+		//Never delete this line!
+		parent::Create();
+
+		/*
+      $this->RegisterVariableInteger("BUTTONS", "Befehl", "SE_IRButtons");
+      $this->EnableAction("BUTTONS");
+
+      $this->RegisterVariableInteger("INPUT", "Input", "SE_IRInput");
+      $this->EnableAction("INPUT");
+      
+			$this->RegisterPropertyString("PORT", 0);
+			$this->RegisterPropertyString("IP", "");
+      $this->RegisterPropertyString("REMOTE", "");       
+*/
+	}
+
+	public function ApplyChanges()
+	{
+		//Never delete this line!
+		parent::ApplyChanges();
+	}
+
+	#        SetValue($this->GetIDForIdent($Ident), $value);        
+
+}
